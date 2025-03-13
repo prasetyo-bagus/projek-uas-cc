f7f7895fd1c379acde230309492c2aa2e3110a9c
Prasetyo Bagus <bagusprasetyo.work@gmail.com>
2025-03-13 23:50:24 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'SUPER ADMIN',
+                'email' => 'superadmin@example.com',
+                'role' => 1,
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('password123'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'ADMIN',
+                'email' => 'admin@example.com',
+                'role' => 2,
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('password123'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified', 'admin'])
+    ->name('dashboard');
+ 
+Route::view('superadmin', 'superadmin')
+->middleware(['auth', 'verified','superadmin'])
+->name('superadmin');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
